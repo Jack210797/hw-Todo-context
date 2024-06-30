91dcbc8b5b43414856606461d6b83c02d00cafef
Jack210797 <suvorov21079@gmail.com>
2024-06-30 14:17:56 +0300

    Todo_List

--- src/data/mock.todos.ts
+++ src/data/mock.todos.ts
@@ -2,55 +2,54 @@ export const MOCK_TODOS = [
   {
     userId: 1,
     id: '1',
-    title: 'delectus aut autem',
+    title: 'Заробити 100000$',
     completed: false
   },
   {
     userId: 1,
     id: '2',
-    title: 'quis ut nam facilis et officia qui',
+    title: 'Практивувати React',
     completed: false
   },
   {
     userId: 1,
     id: '3',
-    title: 'fugiat veniam minus',
+    title: 'Гарно навчатися',
     completed: false
   },
   {
     userId: 1,
     id: '4',
-    title: 'et porro tempora',
+    title: 'Багато гуляти',
     completed: true
   },
   {
     userId: 1,
     id: '5',
-    title: 'laboriosam mollitia et enim quasi adipisci quia provident illum',
-    completed: false
+    title: 'програмувати на JavaScript'
   },
   {
     userId: 1,
     id: '6',
-    title: 'qui ullam ratione quibusdam voluptatem quia omnis',
+    title: 'Читати книги',
     completed: false
   },
   {
     userId: 1,
     id: ' 7',
-    title: 'illo expedita consequatur quia in',
+    title: 'Приготувати піццу',
     completed: false
   },
   {
     userId: 1,
     id: '8',
-    title: 'quo adipisci enim quam ut ab',
+    title: 'Замовити продукти',
     completed: true
   },
   {
     userId: 1,
     id: '9',
-    title: 'molestiae perspiciatis ipsa',
+    title: 'Замовити подарунок для мами',
     completed: false
   }
 ]
